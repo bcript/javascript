31e0c7b30afe1a6b99ca84ba9e49a005fc1342ac
Basil Chin <139629525+bcript@users.noreply.github.com>
2024-07-10 09:10:09 +0800

    Update basics.js

--- basics.js
+++ basics.js
@@ -1,1 +1,33 @@
-code
+// Online Javascript Editor for free
+// Write, Edit and Run your Javascript code using JS Online Compiler
+let myName = 'Python';
+let camelCase = 'Naming Convention';
+myName = 'Name change: JS'; // not required let
+
+const myAge = 18; // constant, cannot be changed
+
+let myArr = [1, 2, 3, 4, 5, 6, 7];
+
+let isHuman  = true;
+let isDeceased = false; // lower case for booleans
+
+// Math //
+let a = (5 + 7) * 2;
+let b = (5 - 7) / 3;
+let c = 5 ** 7;//power
+let d = 123 % 45; // remainder
+console.log(a, b, c, d);
+
+let e = Math.random();
+let f = Math.round(123.456789);
+let g = Math.cos(Math.PI);
+let h = Math.E ** 2;
+console.log(e, f, g, h);
+
+// test //
+console.log(myName);
+console.log(myAge);
+console.log(myArr);
+console.log(isHuman);
+console.log(isDeceased);
+
